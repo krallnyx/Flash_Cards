03ee0bd99e78ee5a3892919431e534147cfff594
Arnaud Jeanroch <arnaud@jeanroch.fr>
2021-03-02 13:49:21 +0000

    #3 Cards will now flip after 3 seconds to see the English translation

--- main.py
+++ main.py
@@ -10,11 +10,15 @@ class FlashCards:
     def __init__(self):
         self.window = Tk()
         self.canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
-        data = pandas.read_csv("data/french_words.csv")
+        try:
+            data = pandas.read_csv("data/words_to_learn.csv")
+        except FileNotFoundError:
+            data = pandas.read_csv("data/french_words.csv")
         self.dict = data.to_dict(orient="records")
         self.card_title = None
         self.card_word = None
         self.create_layout()
+        self.flip_timer = self.window.after(3000, func=self.flip_card)
 
     def create_layout(self):
 
@@ -22,25 +26,41 @@ class FlashCards:
         self.window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
 
         self.canvas.card_front_image = PhotoImage(file="images/card_front.png")
-        self.canvas.create_image(400, 263, image=self.canvas.card_front_image)
+        self.canvas.card_back_image = PhotoImage(file="images/card_back.png")
+        self.card_background = self.canvas.create_image(400, 263)
         self.card_title = self.canvas.create_text(400, 150, text="", font=("Arial", 40, "italic"))
         self.card_word = self.canvas.create_text(400, 263, text="", font=("Arial", 60, "bold"))
         self.canvas.grid(column=0, row=0, columnspan=2)
 
         self.canvas.right_image = PhotoImage(file="images/right.png")
-        right_button = Button(image=self.canvas.right_image, highlightthickness=0, command=self.create_new_card)
+        right_button = Button(image=self.canvas.right_image, highlightthickness=0, command=self.remove_from_list)
         right_button.grid(column=1, row=1)
         self.canvas.wrong_image = PhotoImage(file="images/wrong.png")
         wrong_button = Button(image=self.canvas.wrong_image, highlightthickness=0, command=self.create_new_card)
         wrong_button.grid(column=0, row=1)
         self.create_new_card()
+        self.flip_timer = self.window.after(3000, func=self.flip_card)
 
     def create_new_card(self):
-        self.canvas.itemconfig(self.card_title, text="French")
-        self.canvas.itemconfig(self.card_word, text=random.choice(self.dict)["French"])
+        try:
+            self.window.after_cancel(self.flip_timer)
+        except AttributeError:
+            pass
+        self.current_card = random.choice(self.dict)
+        self.canvas.itemconfig(self.card_background, image=self.canvas.card_front_image)
+        self.canvas.itemconfig(self.card_title, text="French", fill="black")
+        self.canvas.itemconfig(self.card_word, text=self.current_card["French"], fill="black")
+        self.flip_timer = self.window.after(3000, func=self.flip_card)
+
+    def flip_card(self):
+        self.canvas.itemconfig(self.card_title, text="English", fill="white")
+        self.canvas.itemconfig(self.card_word, text=self.current_card["English"], fill="white")
+        self.canvas.itemconfig(self.card_background, image=self.canvas.card_back_image)
+
+    def remove_from_list(self):
+        self.create_new_card()
 
 
 if __name__ == '__main__':
     flash_cards = FlashCards()
-
     flash_cards.window.mainloop()
